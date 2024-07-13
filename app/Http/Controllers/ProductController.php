ebda77427cf2a371fd125b8d28d1d4f93e6d2c58
DeltaKhalid <khalid.delta063@gmail.com>
2024-07-13 13:44:54 +0600

    Read ( R ) data and show from database has been done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,8 @@ class ProductController extends Controller
 {
     //
     public  function index(){
-        return view('product');
+        $products = Product::all();  // "Product" it is the Model name for connect to the database
+        return view('product', ['products' => $products]);
     }
 
     public  function create(){
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -7,5 +7,26 @@
 </head>
 <body>
     <h1>Product Page</h1>
+    <div>
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Qty</th>
+                <th>Price</th>
+                <th>Description</th>
+            </tr>
+            <!-- take data all using "foreach loop"  and show the data-->
+            @foreach ($products as $product)   
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->name}}</td>
+                    <td>{{$product->qty}}</td>
+                    <td>{{$product->price}}</td>
+                    <td>{{$product->discription}}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 </body>
 </html>
